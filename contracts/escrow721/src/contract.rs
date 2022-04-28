ed9bb6d4c26d76c3f249b15ad69af49c7ce5a1f9
Humanalgorithm <scottomj@gmail.com>
2022-04-28 17:12:42 -0400

    fix formatting

--- contracts/escrow721/src/contract.rs
+++ contracts/escrow721/src/contract.rs
@@ -1,12 +1,10 @@
 #[cfg(not(feature = "library"))]
 use cosmwasm_std::entry_point;
-use cosmwasm_std::{ MessageInfo, DepsMut, Env, StdResult, Empty
-};
-use cw721_base::msg::InstantiateMsg;
+use cosmwasm_std::{DepsMut, Empty, Env, MessageInfo, StdResult};
 use cw721_base;
+use cw721_base::msg::InstantiateMsg;
 
 pub type CW721ContractWrapper<'a> = cw721_base::Cw721Contract<'a, Empty, Empty>;
-
 
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn instantiate(
@@ -15,7 +13,5 @@ pub fn instantiate(
     _info: MessageInfo,
     msg: InstantiateMsg,
 ) -> StdResult<cosmwasm_std::Response> {
-
-    CW721ContractWrapper::default()
-    .instantiate(deps, _env, _info, msg)
+    CW721ContractWrapper::default().instantiate(deps, _env, _info, msg)
 }
