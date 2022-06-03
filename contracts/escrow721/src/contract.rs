f2c06f21181c7f2c0c8cc3620eca82955870dbd3
Humanalgorithm <scottomj@gmail.com>
2022-06-02 22:37:28 -0400

    fix lint

--- contracts/escrow721/src/contract.rs
+++ contracts/escrow721/src/contract.rs
@@ -3,12 +3,12 @@ use cosmwasm_std::Response;
 use cosmwasm_std::StdError;
 #[cfg(not(feature = "library"))]
 use cosmwasm_std::{to_binary, Binary, Deps, DepsMut, Empty, Env, MessageInfo, StdResult};
-use cw721_base_ibc::msg::{InstantiateMsg, MintMsg, QueryMsg, ExecuteMsg};
+use cw721_base_ibc::msg::{ExecuteMsg, InstantiateMsg, MintMsg, QueryMsg};
 use cw721_base_ibc::{ContractError, Cw721Contract};
 use cw721_ibc::{Cw721Execute, Cw721Query, NftInfoResponse, OwnerOfResponse};
 
 pub type CW721ContractWrapper<'a> = Cw721Contract<'a, Empty, Empty>;
-use crate::state::{CLASS_STORAGE};
+use crate::state::CLASS_STORAGE;
 
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn instantiate(
@@ -29,17 +29,19 @@ pub fn execute(
 ) -> Result<Response<Empty>, ContractError> {
     println!("we are in execute~!");
     match msg {
-        ExecuteMsg::SaveClass { class_id, class_uri } => save_class(deps, class_id, class_uri),
-        _ =>  CW721ContractWrapper::default().execute(deps, env, info, msg)
+        ExecuteMsg::SaveClass {
+            class_id,
+            class_uri,
+        } => save_class(deps, class_id, class_uri),
+        _ => CW721ContractWrapper::default().execute(deps, env, info, msg),
     }
 }
 
 pub fn save_class(
     deps: DepsMut,
-    class_id: String, 
-    class_uri: String, 
+    class_id: String,
+    class_uri: String,
 ) -> Result<Response<Empty>, ContractError> {
-
     CLASS_STORAGE.save(deps.storage, class_id, &class_uri)?;
     Ok(Response::default())
 }
@@ -99,7 +101,7 @@ pub fn query(deps: Deps, _env: Env, msg: QueryMsg) -> StdResult<Binary> {
             include_expired.unwrap_or(false),
         )?),
         QueryMsg::NftInfo { class_id, token_id } => to_binary(&nft_info(deps, class_id, token_id)?),
-        QueryMsg::HasClass { class_id } => to_binary(&has_class(deps, class_id)), 
+        QueryMsg::HasClass { class_id } => to_binary(&has_class(deps, class_id)),
         _ => Err(StdError::GenericErr {
             msg: "Unsupported message type".to_string(),
         }),
