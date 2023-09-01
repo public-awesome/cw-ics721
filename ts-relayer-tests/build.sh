e23c83fb55022dc9f9df3d3f9b896f0dc145cb58
mr-t <mr-t@arkprotocol.io>
2023-09-01 20:24:23 +0200

    fix ts-relayer tests

--- ts-relayer-tests/build.sh
+++ ts-relayer-tests/build.sh
@@ -11,7 +11,7 @@ cd "$(git rev-parse --show-toplevel)"
 docker run --rm -v "$(pwd)":/code --platform linux/amd64 \
 	--mount type=volume,source="$(basename "$(pwd)")_cache",target=/code/target \
 	--mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
-	cosmwasm/workspace-optimizer:0.12.11
+	cosmwasm/workspace-optimizer:0.14.0
 
 mkdir -p ./ts-relayer-tests/internal
 cp ./artifacts/*.wasm ./ts-relayer-tests/internal
