f43f9c088fa0b4b69ff5cc2046eec08a0d5fbe8d
mr-t <mr-t@arkprotocol.io>
2024-01-28 00:28:57 +0100

    move to dedicated functions

--- packages/ics721/src/ibc_packet_receive.rs
+++ packages/ics721/src/ibc_packet_receive.rs
@@ -1,6 +1,6 @@
 use cosmwasm_std::{
-    from_json, to_json_binary, Addr, DepsMut, Empty, Env, IbcPacket, IbcReceiveResponse, StdResult,
-    SubMsg, WasmMsg,
+    from_json, to_json_binary, Addr, Binary, Deps, DepsMut, Empty, Env, IbcPacket,
+    IbcReceiveResponse, StdResult, SubMsg, WasmMsg,
 };
 use sha2::{Digest, Sha256};
 use zip_optional::Zippable;
@@ -31,10 +31,6 @@ pub(crate) fn receive_ibc_packet(
     let data: NonFungibleTokenPacketData = from_json(&packet.data)?;
     data.validate()?;
 
-    let cloned_data = data.clone();
-    let receiver = deps.api.addr_validate(&data.receiver)?;
-    let token_count = data.token_ids.len();
-
     // Check if NFT is local if not get the local class id
     let maybe_local_class_id = try_pop_source_prefix(&packet.src, &data.class_id);
     let callback = get_receive_callback(&data);
@@ -46,6 +42,59 @@ pub(crate) fn receive_ibc_packet(
         ClassId::new(format!("{}{}", local_prefix, data.class_id))
     };
 
+    let incoming_proxy_msg =
+        get_incoming_proxy_msg(deps.as_ref().storage, packet.clone(), data.clone())?;
+
+    let (is_redemption, voucher_and_channel_messages) = create_voucher_and_channel_messages(
+        deps.as_ref(),
+        env.clone(),
+        data.clone(),
+        maybe_local_class_id,
+        local_class_id.clone(),
+        packet.clone(),
+    )?;
+
+    // if there is a callback, generate the callback message
+    let callback_msg = create_callback_msg(
+        deps.as_ref(),
+        &env,
+        &data,
+        is_redemption,
+        callback,
+        local_class_id,
+    )?;
+
+    let submessage = into_submessage(
+        env.contract.address,
+        voucher_and_channel_messages.0,
+        voucher_and_channel_messages.1,
+        callback_msg,
+        incoming_proxy_msg,
+    )?;
+
+    let response = if let Some(memo) = data.memo {
+        IbcReceiveResponse::default().add_attribute("ics721_memo", memo)
+    } else {
+        IbcReceiveResponse::default()
+    };
+
+    Ok(response
+        .add_submessage(submessage)
+        .add_attribute("method", "receive_ibc_packet")
+        .add_attribute("class_id", data.class_id)
+        .add_attribute("local_channel", packet.dest.channel_id)
+        .add_attribute("counterparty_channel", packet.src.channel_id))
+}
+
+fn create_voucher_and_channel_messages(
+    deps: Deps,
+    env: Env,
+    data: NonFungibleTokenPacketData,
+    maybe_local_class_id: Option<&str>,
+    local_class_id: ClassId,
+    packet: IbcPacket,
+) -> Result<(bool, (WasmMsg, WasmMsg)), ContractError> {
+    let token_count = data.token_ids.len();
     let redemption_or_create = data
         .token_ids
         .into_iter()
@@ -86,7 +135,6 @@ pub(crate) fn receive_ibc_packet(
                 Ok(redemption_or_create)
             },
         )?;
-
     let is_redemption = if !redemption_or_create.0.is_empty() && !redemption_or_create.1.is_empty()
     {
         // All token ids in the transfer must be either a redeption or creation
@@ -101,49 +149,7 @@ pub(crate) fn receive_ibc_packet(
         return Err(ContractError::InvalidTransferNoAction);
     };
 
-    // if there is a callback, generate the callback message
-    let callback_msg = if let Some((receive_callback_data, receive_callback_addr)) = callback {
-        // callback require the nft contract, get it using the class id from the action
-        let nft_contract = if is_redemption {
-            // If its a redemption, it means we already have the contract address in storage
-
-            CLASS_ID_TO_NFT_CONTRACT
-                .load(deps.storage, local_class_id.clone())
-                .map_err(|_| ContractError::NoNftContractForClassId(local_class_id.to_string()))
-        } else {
-            // If its a creation action, we can use the instantiate2 function to get the nft contract
-            // we don't care of the contract is instantiated yet or not, as later submessage will instantiate it if its not.
-            // The reason we use instantiate2 here is because we don't know if it was already instantiated or not.
-
-            let cw721_code_id = CW721_CODE_ID.load(deps.storage)?;
-            // for creating a predictable nft contract using, using instantiate2, we need: checksum, creator, and salt:
-            // - using class id as salt for instantiating nft contract guarantees a) predictable address and b) uniqueness
-            // for this salt must be of length 32 bytes, so we use sha256 to hash class id
-            let mut hasher = Sha256::new();
-            hasher.update(local_class_id.as_bytes());
-            let salt = hasher.finalize().to_vec();
-
-            get_instantiate2_address(
-                deps.as_ref(),
-                env.contract.address.as_str(),
-                &salt,
-                cw721_code_id,
-            )
-        }?;
-
-        generate_receive_callback_msg(
-            deps.as_ref(),
-            &cloned_data,
-            receive_callback_data,
-            receive_callback_addr,
-            nft_contract.to_string(),
-        )
-    } else {
-        None
-    };
-
-    let incoming_proxy_msg =
-        get_incoming_proxy_msg(deps.storage, packet.clone(), cloned_data.clone())?;
+    let receiver = deps.api.addr_validate(&data.receiver)?;
     let voucher_and_channel_messages = match is_redemption {
         true => {
             let redemption = VoucherRedemption {
@@ -206,26 +212,51 @@ pub(crate) fn receive_ibc_packet(
         }
     };
 
-    let submessage = into_submessage(
-        env.contract.address,
-        voucher_and_channel_messages.0,
-        voucher_and_channel_messages.1,
-        callback_msg,
-        incoming_proxy_msg,
-    )?;
+    Ok((is_redemption, voucher_and_channel_messages))
+}
 
-    let response = if let Some(memo) = data.memo {
-        IbcReceiveResponse::default().add_attribute("ics721_memo", memo)
+fn create_callback_msg(
+    deps: Deps,
+    env: &Env,
+    data: &NonFungibleTokenPacketData,
+    is_redemption: bool,
+    callback: Option<(Binary, Option<String>)>,
+    local_class_id: ClassId,
+) -> Result<Option<WasmMsg>, ContractError> {
+    if let Some((receive_callback_data, receive_callback_addr)) = callback {
+        // callback require the nft contract, get it using the class id from the action
+        let nft_contract = if is_redemption {
+            // If its a redemption, it means we already have the contract address in storage
+
+            CLASS_ID_TO_NFT_CONTRACT
+                .load(deps.storage, local_class_id.clone())
+                .map_err(|_| ContractError::NoNftContractForClassId(local_class_id.to_string()))
+        } else {
+            // If its a creation action, we can use the instantiate2 function to get the nft contract
+            // we don't care of the contract is instantiated yet or not, as later submessage will instantiate it if its not.
+            // The reason we use instantiate2 here is because we don't know if it was already instantiated or not.
+
+            let cw721_code_id = CW721_CODE_ID.load(deps.storage)?;
+            // for creating a predictable nft contract using, using instantiate2, we need: checksum, creator, and salt:
+            // - using class id as salt for instantiating nft contract guarantees a) predictable address and b) uniqueness
+            // for this salt must be of length 32 bytes, so we use sha256 to hash class id
+            let mut hasher = Sha256::new();
+            hasher.update(local_class_id.as_bytes());
+            let salt = hasher.finalize().to_vec();
+
+            get_instantiate2_address(deps, env.contract.address.as_str(), &salt, cw721_code_id)
+        }?;
+
+        Ok(generate_receive_callback_msg(
+            deps,
+            data,
+            receive_callback_data,
+            receive_callback_addr,
+            nft_contract.to_string(),
+        ))
     } else {
-        IbcReceiveResponse::default()
-    };
-
-    Ok(response
-        .add_submessage(submessage)
-        .add_attribute("method", "receive_ibc_packet")
-        .add_attribute("class_id", data.class_id)
-        .add_attribute("local_channel", packet.dest.channel_id)
-        .add_attribute("counterparty_channel", packet.src.channel_id))
+        Ok(None)
+    }
 }
 
 pub fn into_submessage(
